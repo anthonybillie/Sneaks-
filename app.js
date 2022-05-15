4a30450ef2686312581e6afdd4b4ab0bb66589b8
anthonybillie <anthonymbillie@gmail.com>
2022-05-15 14:40:36 -0400

    js file updated with fetch info

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'the-sneaker-database.p.rapidapi.com',
+    'X-RapidAPI-Key': '1917ad3211msh4712239feeda776p1e22c8jsndc955214cb00'
+  }}
+
+
+fetch('https://the-sneaker-database.p.rapidapi.com/sneakers?limit=10', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
