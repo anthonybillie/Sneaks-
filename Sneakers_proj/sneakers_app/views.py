5a8cd292d1bcee462d01fcf73c67ad0874461f18
anthonybillie <anthonymbillie@gmail.com>
2022-05-18 23:42:15 -0400

    project update

--- Sneakers_proj/sneakers_app/views.py
+++ Sneakers_proj/sneakers_app/views.py
@@ -1,10 +1,12 @@
+from urllib import response
 from django.shortcuts import render
-from .import models
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import requests as fetch
 
 # Create your views here.
 
 
 def index(request):
+
     return render(request, 'sneakers_app/index.html')
