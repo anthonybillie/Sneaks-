6e1a1221a3e803b6188c7dd8176f1d614973ffaf
anthonybillie <anthonymbillie@gmail.com>
2022-06-23 14:04:42 -0400

    updates

--- Sneakers_proj/Sneakers_proj/settings.py
+++ Sneakers_proj/Sneakers_proj/settings.py
@@ -126,5 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 django_on_heroku.settings(locals())
